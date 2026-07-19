914587d33aa9a00fa3599acd5a0ef05e6d1db736
shreesinghal <119455146+shreesinghal@users.noreply.github.com>
2026-07-19 11:44:25 -0400

    Add: top-k-elements-in-list - submission-7

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-7.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> counts; // counts[a] = b where a appears a times in nums
+        vector<vector<int>> frequency(nums.size() +1); // frequency[i] = lst where every item in lst appears i times in nums
+
+        // populate counts
+        for (int number: nums) 
+            counts[number]++;
+
+        // populate frequency
+        for (auto couple : counts) {
+            frequency[couple.second].push_back(couple.first);
+        }
+
+        vector<int> topK = {};
+
+        for (int i = frequency.size() - 1; i >= 0 && k > 0; i--) {
+            for (int j = frequency[i].size() - 1; j >= 0 && k > 0; j--) {
+                if (k) {
+                    topK.push_back(frequency[i][j]);
+                    k--;
+                }
+                else {
+                    return topK;
+                }
+            }
+        }
+
+        return topK;
+    }
+};
